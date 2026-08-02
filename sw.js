9276eb731e84837a8074bcea5d2bf86d960ed53d
dumplingslove <darancai@gmail.com>
2026-08-02 00:57:03 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Service Worker for PWA install support
+const CACHE = 'card-optimizer-v5';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
+  self.skipWaiting();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
